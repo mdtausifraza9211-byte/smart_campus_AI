6f2118c2171989a9f5f162e038eea163e2fce049
mdtausifraza9211-byte <mdtausif843109@gmail.com>
2026-09-03 09:45:48 +0530

    Update app.py
    Smart Campus AI Assistant is an AI-powered web application designed to help students quickly access important campus information. It provides information about campus locations such as the ground, workshop, physics lab, chemistry lab, library, computer lab, canteen, seminar hall, and administrative offices. The system allows students to ask questions in simple language and receive quick answers. The project is built using Python, Flask, HTML, CSS, and JavaScript.

--- app.py
+++ app.py
@@ -3,11 +3,23 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 campus_info = {
-    "computer lab": "The computer lab is located in Block A, first floor.",
+    "computer lab": "The computer lab is located in CST Block, Ground floor.",
     "library": "The library is located in the main academic building.",
     "canteen": "The canteen is near the boy hostel.",
-    "principal": "The Principal's office is located in the administration building.",
-    "office": "The administration office is located in the main building."
+    "principal office": "The Principal's office is located in the administration building.",
+    "Aminstration office": "The administration office is located in the administration building.",
+    "Registrar office":"The Registrar office is located in the administration building",
+    "Finance office":"The Finance office is located in the administration building",
+    "Examination Cell":"The Examination Cell is located in the administration building",
+    "Basic Electrical lab":"The Basic Electrical lab is located in the CORE block,second floor ",
+    "Physics lab":"The Physics lab is located behid central library,second floor",
+    "Chemistry lab":"The chemistry lab is located in the CORE Block,Ground floor",
+    "Engineering Drowing lab":"The Engineering Drawing lab is located in the CORE Block, second floor",
+    "Analog and Digital Electronics lab":"The Analog and Digital Electronics lab is located in the CORE Block,first floor",
+    "Computer Organization lab":"The Computer Organization lab is located in CORE Block,first floor",
+    "General store":"The General store is located at Exit gate of Campus",
+    "Seminar Hall":"The Seminar Hall is located in CST Block, First Floor",
+    "Workshop":"Workshop is located in CORE Block, Ground Floor",
 }
 
 @app.route("/")
